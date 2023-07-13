b2110c7e546195aa9b7e51abd1072845c3c29d0b
JoyAsherOmenogor <133912827+JoyAsherOmenogor@users.noreply.github.com>
2023-07-12 22:27:36 -0400

    Update old_people.py

--- old_people.py
+++ old_people.py
@@ -6,13 +6,15 @@ from create_db import db_path, script_dir
 def main():
     old_people_list = get_old_people()
     print_name_and_age(old_people_list)
-    save_name_and_age_to_csv(old_people_list)
+
+    old_people_csv = os.path.join(script_dir, 'old_people.csv')
+    save_name_and_age_to_csv(old_people_list, old_people_csv)
 
 def get_old_people():
     """Queries the Social Network database for all people who are at least 50 years old.
 
     Returns:
-        list: List of dictionaries containing name and age of old people
+        list: List of tuples containing (name, age) of old people 
     """
     con = sqlite3.connect(db_path)
     cur = con.cursor()
@@ -25,38 +27,32 @@ def get_old_people():
     old_people_list = cur.fetchall()
 
     con.close()
-
-    # Convert the list of tuples to a list of dictionaries
-    old_people_list = [{'name': row[0], 'age': row[1]} for row in old_people_list]
-
     return old_people_list
 
 def print_name_and_age(name_and_age_list):
-    """Prints the name and age of all people in the provided list.
+    """Prints name and age of all people in the provided list.
 
     Args:
-        name_and_age_list (list): List of dictionaries containing name and age information
+        name_and_age_list (list): List of tuples containing (name, age) of people
     """
     for person in name_and_age_list:
-        name = person['name']
-        age = person['age']
+        name, age = person
         print(f"{name} is {age} years old.")
 
-def save_name_and_age_to_csv(name_and_age_list):
-    """Saves the name and age of all people in the provided list to a CSV file.
+def save_name_and_age_to_csv(name_and_age_list, csv_path):
+    """Saves name and age of all people in the provided list to a CSV file.
 
     Args:
-        name_and_age_list (list): List of dictionaries containing name and age information
+        name_and_age_list (list): List of tuples containing (name, age) of people
+        csv_path (str): Path of the CSV file
     """
-    csv_path = os.path.join(script_dir, 'old_people.csv')
-    
     with open(csv_path, 'w', newline='') as csvfile:
         fieldnames = ['Name', 'Age']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        
+
         writer.writeheader()
-        writer.writerows(name_and_age_list)
+        for person in name_and_age_list:
+            writer.writerow({'Name': person[0], 'Age': person[1]})
 
 if __name__ == '__main__':
    main()
-
